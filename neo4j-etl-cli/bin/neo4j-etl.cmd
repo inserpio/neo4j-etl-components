505b4223b2e5b27158b70e5f48eaef6d1be7e48e
Lorenzo Speranzoni <lorenzo.speranzoni@larus-ba.it>
2017-05-30 20:03:39 +0200

    shell scripts + maven profiles for IT + documentation updated (#24)
    * added support for oracle. fixes #18.
    * maven profiles reviewed for correct tests execution + added instructions to install oracle driver into the local mvan repository
    * changed shell script to include driver jars in the classpath + reviewed documentation accordingly
    * mysql docker documentation updated

--- neo4j-etl-cli/bin/neo4j-etl.cmd
+++ neo4j-etl-cli/bin/neo4j-etl.cmd
@@ -1,6 +1,9 @@
 @echo off
-
-setlocal
-	dir neo4j-etl.jar /b/s>temp
-	set /p jar=<temp
-endlocal & java -jar %jar% %*
+set jarpath=%0
+set jarpath=%jarpath:\neo4j-etl=%\..
+setlocal enabledelayedexpansion
+	set jars=
+    for /f "delims=" %%a in ('dir %jarpath%\lib\*.jar /s/b') do set jars=%%a;!jars!
+	set jars=!jars:~0,-1!
+	echo %jars%
+endlocal & java -classpath %jars% org.neo4j.etl.NeoIntegrationCli %*
